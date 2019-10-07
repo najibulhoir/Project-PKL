f05d9a2d6f7dcb26fe79dbda47f7b43b145b9639
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-07 11:11:11 +0700

    Halo

---
+++ application/controllers/Template.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Template extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  function index()
+  {
+
+    $this->load->view('admin/header.php');
+    $this->load->view('admin/sidebar.php');
+    $this->load->view('admin/footer.php');
+  }
+
+}
