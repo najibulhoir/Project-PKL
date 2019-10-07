47cac2949685cecc5e4c2b0ce56af50666aec670
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-07 11:20:14 +0700

    .

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Template extends CI_Controller{
+class Admin extends CI_Controller{
 
   public function __construct()
   {
