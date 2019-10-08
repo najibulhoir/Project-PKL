c8dba527b012080ad73c45501ee8f5bb8aa72973
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-08 16:11:57 +0700

    Update d_pegawai.php

--- application/views/admin/d_pegawai.php
+++ application/views/admin/d_pegawai.php
@@ -371,32 +371,35 @@
 					         
 					            </tbody>
 					        </table>
-					        
+
 					        <!-- Tombol untuk menampilkan modal-->
-	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Tambah Pegawai</button>
- 
-	<!-- Modal -->
-	<div id="myModal" class="modal fade" role="dialog">
-		<div class="modal-dialog">
-			<!-- konten modal-->
-			<div class="modal-content">
-				<!-- heading modal -->
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">Bagian heading modal</h4>
-				</div>
-				<!-- body modal -->
-				<div class="modal-body">
-					<p>bagian body modal.</p>
-				</div>
-				<!-- footer modal -->
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Tutup Modal</button>
-				</div>
-			</div>
-		</div>
-	</div>
-   </div>
+							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Tambah</button>
+						 
+							<!-- Modal -->
+							<div id="myModal" class="modal fade" role="dialog">
+								<div class="modal-dialog">
+									<!-- konten modal-->
+									<div class="modal-content">
+										<!-- heading modal -->
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal">&times;</button>
+											<h4 class="modal-title">Tambah Pegawai</h4>
+										</div>
+										<!-- body modal -->
+										<div class="modal-body">
+										
+
+												
+											</form>
+										</div>
+										<!-- footer modal -->
+										<div class="modal-footer">
+											<button type="button" class="btn btn-default" data-dismiss="modal">Tutup Modal</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						   </div>
 					    </div>
 					</div>
 					<!--===================================================-->
