3bc750221bca1a3b63906eb13e4b0ad2edb740be
AldaGheaulyHastari <44992469+AldaGheaulyHastari@users.noreply.github.com>
2019-10-10 12:21:49 +0700

    su

--- application/modules/dashboard/views/template/sidebar.php
+++ application/modules/dashboard/views/template/sidebar.php
@@ -671,52 +671,103 @@
 
                     <!--Menu list item-->
                     <li class="active-sub">
-                        <a href="<?= base_url().'Dashboard/karyawan' ?>">
+                        <a href="<?= base_url().'Dashboard' ?>">
                             <i class="fa fa-home"></i>
                             <span class="menu-title">
-                    Beranda
-                  </span>
+                                Beranda
+                            </span>
+                        </a>
+                    </li>
+
+                    <!--Menu list item-->
+                    <li >
+                        <a href="<?= base_url().'toko' ?>">
+                            <i class="glyphicon glyphicon-book"></i>
+                            <span class="menu-title">Data Toko</span>
+                            <i class="arrow"></i>
+                        </a>
+
+                        <!--Submenu-->
+                        <ul class="collapse">
+                           <li><a href="<?= base_url().'toko' ?>"><i class="fa fa-edit"></i>
+               <span class="menu-title">Informasi Toko
+                           <li><a href="<?= base_url().'faqs' ?>"><i class="fa fa-edit"></i>Faqs</a></li>
+                        </ul>
+                    </li>
 
                     <!--Menu item-->
-                                <li>
-                                    <a href="<?= base_url().'Barang/k_barang' ?>">
-                                        <i class="fa fa-file"></i>
-                                        <span class="menu-title">
-                                            Data Barang
-                                            <span class="pull-right badge badge-warning"></span>
-                                        </span>
-                                    </a>
-                                </li>
-
-                                 <!--Menu item-->
-                                <li>
-                                    <a href="<?= base_url().'Barang_Ready/kbarangre' ?>">
-                                        <i class="fa fa-history"></i>
-                                        <span class="menu-title">
-                                            Barang Ready
-                                            <span class="pull-right badge badge-warning"></span>
-                                        </span>
-                                    </a>
-                                </li>
+                    <li>
+                        <a href="<?= base_url().'barang' ?>">
+                            <i class="fa fa-file"></i>
+                            <span class="menu-title">
+                                Data Barang
+                                <span class="pull-right badge badge-warning"></span>
+                            </span>
+                        </a>
+                    </li>
 
                      <!--Menu item-->
-                                <li>
+                    <li>
+                        <a href="<?= base_url().'riwayat' ?>">
+                            <i class="fa fa-history"></i>
+                            <span class="menu-title">
+                                Riwayat Barang
+                                <span class="pull-right badge badge-warning"></span>
+                            </span>
+                        </a>
+                    </li>
 
-                                    <a href="<?= base_url().'Riwayat/kRiwayat' ?>">
+                     <!--Menu item-->
+                    <li>
+                        <a href="<?= base_url().'Barang_Ready' ?>">
+                            <i class="glyphicon glyphicon-list-alt"></i>
+                            <span class="menu-title">
+                                Barang Ready
+                                <span class="pull-right badge badge-warning"></span>
+                            </span>
+                        </a>
+                    </li>
 
-                                    <a href="<?= base_url().'Barang_Ready' ?>">
+                       <!--Menu item-->
+                    <li>
+                        <a href="<?= base_url().'pegawai' ?>">
+                            <i class="fa fa-file"></i>
+                            <span class="menu-title">
+                                Data Pegawai
+                                <span class="pull-right badge badge-warning"></span>
+                            </span>
+                        </a>
+                    </li>
 
-                                        <i class="glyphicon glyphicon-list-alt"></i>
-                                        <span class="menu-title">
-                                            Riwayat Barang
-                                            <span class="pull-right badge badge-warning"></span>
-                                        </span>
-                                    </a>
+                     <!--Menu list item-->
+                    <li >
+                        <a href="<?= base_url().'penjualan' ?>">
+                            <i class="glyphicon glyphicon-book"></i>
+                            <span class="menu-title">Laporan</span>
+                            <i class="arrow"></i>
+                        </a>
+
+                        <!--Submenu-->
+                        <ul class="collapse">
+                                       <li><a href="<?= base_url().'penjualan' ?>"><i class="fa fa-book"></i>
+                           <span class="menu-title">Penjualan
+                                       <li><a href="<?= base_url().'pembelian' ?>"><i class="fa fa-book"></i>Pembelian</a></li>
+                                    </ul>
                                 </li>
-                        </div>
-                    </div>
-                </div>
-                <!--================================-->
-                <!--End menu-->
+                     <!--Menu item-->
+                    <li>
+                        <a href="<?= base_url().'kritik' ?>">
+                            <i class="fa fa-wechat"></i>
+                            <span class="menu-title">
+                                Kritik & Saran
+                                <span class="pull-right badge badge-warning"></span>
+                            </span>
+                        </a>
+                    </li>
             </div>
-        </nav>
+        </div>
+    </div>
+    <!--================================-->
+    <!--End menu-->
+  </div>
+  </nav>
