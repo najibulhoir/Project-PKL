d66d1cf5d711af2e1968f358096f566dd3979a5d
Maulidiyawati <48039203+maulidiya@users.noreply.github.com>
2019-10-10 15:35:15 +0700

    Update index.php

--- application/modules/dashboard/views/admin/index.php
+++ application/modules/dashboard/views/admin/index.php
@@ -2,8 +2,8 @@
                 <div id="page-head">
 
 <div class="pad-all text-center">
-    <h3>Welcome back to the Dashboard.</h3>
-    <p1>Scroll down to see quick links and overviews of your Server, To do list, Order status or get some Help using Nifty.<p></p>
+    <h3>Selamat Datang</h3>
+    <p1>Admin<p></p>
 </p1></div>
                     </div>
 
