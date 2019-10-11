b2e335028baf205b22090d45ebfb68eefeaf944a
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-11 08:51:36 +0700

    Update c_dashboard.php

--- application/modules/dashboard/views/customer/c_dashboard.php
+++ application/modules/dashboard/views/customer/c_dashboard.php
@@ -25,7 +25,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<link href="<?php echo base_url();?>assets/web/css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
 	<!--//pop-up-box-->
 	<!-- price range -->
-	<link rel="stylesheet" type="text/css" href="css/jquery-ui1.css">
+	<link rel="stylesheet" type="text/css" href="assets/web/css/jquery-ui1.css">
 	<!-- fonts -->
 	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
 </head>
