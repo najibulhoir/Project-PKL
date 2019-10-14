b387b9ccccd9fe498b27adaac80cce40fc519e14
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-14 10:28:32 +0700

    Update M_barang.php

---
+++ application/modules/Barang/models/M_barang.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_barang extends CI_Model {
+
+	// get data produsen
+	function tampilDatabarang()
+	{
+		 $this->db->select('*');
+		 $this->db->from('tm_detail_barang');
+		 $this->db->join('tm_barang','tm_barang.kd_barang=tm_detail_barang.kd_detail');
+		 $query = $this->db->get();
+		 return $query;
+		}    
+
+	function input_data($data,$table)
+  {
+		$this->db->insert($table,$data);
+	} 
+}
