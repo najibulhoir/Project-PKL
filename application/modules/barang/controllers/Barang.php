1e5867315c950fc2899250d991d6e488dbcb5e62
Teddi_Juniarl <tedyjuniarl@gmail.com>
2019-10-14 10:34:04 +0700

    /

--- application/modules/barang/controllers/Barang.php
+++ application/modules/barang/controllers/Barang.php
@@ -1,62 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-  
+
 class Barang extends CI_Controller{
 
-  function __construct()
+  public function __construct()
   {
-    parent:: __construct();
-    
-    // load model 
-
-    $this->load->model('M_barang');
-
+    parent::__construct();
+    //Codeigniter : Write Less Do More
   }
 
   function index()
   {
-   
-   $data['barang'] = $this->M_barang->tampilDatabarang()->result();
-    $this->load->view('d_barang',$data);
-
+    $this->load->view('d_Barang.php');
   }
-
-  function tambah_barang()
+  function kbarang()
   {
-    
-    $KD = $this->input->post('Kode_Barang');
-    $NB = $this->input->post('Nama_Barang');
-    $K = $this->input->post('Kategori');
-    $Stk = $this->input->post('Stok');
-    $Hrg = $this->input->post('Harga');
-    $Dn= $this->input->post('Diskon');
-    $Ss = $this->input->post('Status');
-
-    $config['max_size']=2048;
-    $config['allowed_types']="png|jpg|jpeg|gif";
-    $config['remove_spaces']=TRUE;
-    $config['overwrite']=TRUE;
-    $config['upload_path']=FCPATH.'assets/adminlte/images/';
-
-    $this->load->library('upload');
-    $this->upload->initialize($config);
-
-    //ambil data image
-    $this->upload->do_upload('foto');
-    $data_image=$this->upload->data('file_name');
-    $pict=$data_image;
- 
-    $data = array(
-      'Kode_Barang' => $KD,
-      'Nama_Barang' => $NB,
-      'Kategori' => $K,
-      'Stok' => $Stk,
-      'Harga' => $Hrg,
-      'Diskon' => $Dn,
-      'Status' => $Ss,
-      'foto' => $pict );
-
-    $this->M_barang->input_data($data,'tm_barang');
-    redirect('Barang');
+  	$this->load->view('k_barang');
   }
 }
